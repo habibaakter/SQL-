ef96e5175f61362a8fd1422c19e057951d96b3d3
habiba jeba <30732188+habibaakter@users.noreply.github.com>
2025-10-24 13:24:33 +0100

    Add files via upload

---
+++ wheresql-statements.sql
@@ -0,0 +1,24 @@
+
+SELECT *
+FROM parks_and_recreation.employee_demographics
+where gender='female'
+;
+SELECT *
+FROM parks_and_recreation.employee_demographics
+WHERE birth_date=>'1990'
+OR NOT gender='male'
+;
+SELECT * 
+FROM parks_and_recreation.employee_demographics
+WHERE (first_name='lislie' AND age=44) OR  age>=55
+;
+
+
+SELECT *
+FROM parks_and_recreation.employee_demographics
+WHERE first_name LIKE 'a__'
+;
+
+SELECT *
+FROM parks_and_recreation.employee_demographics
+WHERE first_name LIKE 'a%'
