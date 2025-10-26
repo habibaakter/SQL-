cdc2f8d11edfca451be2aa69251aad9f41dbeb29
habiba jeba <30732188+habibaakter@users.noreply.github.com>
2025-10-26 18:11:45 +0000

    Update wheresql-statements.sql

--- wheresql-statements.sql
+++ wheresql-statements.sql
@@ -12,8 +12,11 @@ SELECT *
 FROM parks_and_recreation.employee_demographics
 WHERE (first_name='lislie' AND age=44) OR  age>=55
 ;
-
-
+#We can use WHERE clause with date value also
+SELECT *
+FROM employee_demographics
+WHERE birth_date > '1985-01-01';
+-- _ means a specific value
 SELECT *
 FROM parks_and_recreation.employee_demographics
 WHERE first_name LIKE 'a__'
